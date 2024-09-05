7b66a3ee2994d2c59928fe93afa1fa47da751827
WFriver <wrivera@quickapps.mx>
2024-09-05 18:42:46 -0500

    Creacion de funcion
    Entender el funcionamiento de una funcion

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,11 @@
+let length;
+let width;
+
+//Crear funcion, y dar valores apartir de la entrada del usuario:
+
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+
+    let area = length * width;
+   }
