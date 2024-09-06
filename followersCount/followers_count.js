69059db92ab604901ab3db58dd3d126a4c402318
WFriver <wrivera@quickapps.mx>
2024-09-06 18:15:43 -0500

    JavaScript docuemnt for followers
    JavaScript that count how many counts are in the page

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -2,6 +2,7 @@ let count = 0; // Initialize count to 0
 function increaseCount(){
     count ++; // Increment the count by 1
     displayCount();
+    checkCountValue();
 }
 
 function displayCount(){
