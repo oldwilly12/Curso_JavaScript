ccf31faf398bff0468ae5338f3cd3bec7fd0d9ee
WFriver <wrivera@quickapps.mx>
2024-09-17 18:44:16 -0500

    Axios
    installation of axios

---
+++ Axios/axios.js
@@ -0,0 +1,13 @@
+// Import Axios
+const axios = require('axios');
+
+// Make a GET request
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    // Handle successful response
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    // Handle error
+    console.error('Error:', error);
+  });
